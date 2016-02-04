b76c0f6ae0fcd8a06ac6772155360cc15d186834
roberthitchens3 <rjhitchens3@gmail.com>
2016-02-04 15:52:44 -0500

    company tests and user interface thing

--- src/main/java/io/zipcoder/Company.java
+++ src/main/java/io/zipcoder/Company.java
@@ -9,21 +9,74 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Company {
+    private String option = "q to quit\nc to create employee\nf to fire employee\n p to pay employees for their hard work.";
     private ArrayList<Employee> employees;
     public Company(){
         employees = new ArrayList<Employee>();
+        runManger();
     }
     public void runManger(){
-
+        System.out.println(option);
+        String in = userInput("what would you like to do? ", "");
+        while(!in.equals("q")){
+            switch(in){
+                case "c":
+                   employees.add(   createEmployee(userInput("enter employee name:", ""),
+                                    userInput("enter position type: ", ""),
+                                    Double.parseDouble(userInput("enter rate or 0 if salary based: ", "number")),
+                                    Double.parseDouble(userInput("Enter salary or 0 if rate based: ", "number")))
+                                );
+                    break;
+                case "f":
+                    fireEmployee(userInput("enter employee name: ", ""));
+                    break;
+                case "p":
+                    payEmployees();
+                    break;
+                default:
+                    System.out.println("not an option");
+                    break;
+            }
+            System.out.println(option);
+            in = userInput("what would you like to do? ", "");
+        }
     }
-    public void createEmployee(){
+    public Employee createEmployee(String name, String type, double rate, double salary){
+        Employee nuEmp;
+        if(type.equals("manager")){
+            nuEmp = new Manager();
 
+        }else if(type.equals("executive")){
+            nuEmp = new Executive();
+        }else if(type.equals("hourly")){
+            nuEmp = new HourlyEmployee();
+        }else if(type.equals("salaried")){
+            nuEmp = new SalariedEmployee();
+        }else{
+            System.out.println("erroe");
+            nuEmp = null;
+        }
+        nuEmp.setName(name);
+        nuEmp.setRate(rate);
+        nuEmp.setSalary(salary);
+        return nuEmp;
     }
-    public Employee fireEmployee(){
-        return null;
+    public Employee fireEmployee(String eName){
+        Employee removed = null;
+        for(Employee e : employees){
+            if(e.getName().equals(eName)){
+                removed = e;
+                employees.remove(e);
+            }
+        }
+        String mess = (removed==null) ? "could not find employee or employee does not exist.":"successful";
+        System.out.println(mess);
+        return removed;
     }
     public void payEmployees(){
-
+        for(Employee e : employees){
+            System.out.println(e.getName() + " was payed: "+e.calcPay());
+        }
     }
     public ArrayList<Employee> getEmployees(){
         return employees;
