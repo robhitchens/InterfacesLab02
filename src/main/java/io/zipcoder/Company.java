c0431576d1372c113c33ff9b4ed640e1a3fb9ddd
roberthitchens3 <rjhitchens3@gmail.com>
2016-02-04 16:13:50 -0500

    minor update to company

--- src/main/java/io/zipcoder/Company.java
+++ src/main/java/io/zipcoder/Company.java
@@ -9,7 +9,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Company {
-    private String option = "q to quit\nc to create employee\nf to fire employee\n p to pay employees for their hard work.";
+    public static void main(String[] args) {
+        Company company = new Company();
+    }
+    private String option = "q to quit\nc to create employee\nf to fire employee\npr to list employees and their position\np to pay employees for their hard work.";
     private ArrayList<Employee> employees;
     public Company(){
         employees = new ArrayList<Employee>();
@@ -21,17 +24,24 @@ public class Company {
         while(!in.equals("q")){
             switch(in){
                 case "c":
-                   employees.add(   createEmployee(userInput("enter employee name:", ""),
+                   employees.add(   createEmployee(userInput("enter employee name:", ""),//refasctor
                                     userInput("enter position type: ", ""),
                                     Double.parseDouble(userInput("enter rate or 0 if salary based: ", "number")),
                                     Double.parseDouble(userInput("Enter salary or 0 if rate based: ", "number")))
                                 );
                     break;
                 case "f":
-                    fireEmployee(userInput("enter employee name: ", ""));
+                    try {
+                        fireEmployee(userInput("enter employee name: ", ""));
+                    }catch(NullPointerException e){
+                        System.out.println("check employee list before trying to remove employee.");
+                    }
                     break;
                 case "p":
                     payEmployees();
+                    break;
+                case "pr":
+                    printEmployees();
                     break;
                 default:
                     System.out.println("not an option");
@@ -45,7 +55,6 @@ public class Company {
         Employee nuEmp;
         if(type.equals("manager")){
             nuEmp = new Manager();
-
         }else if(type.equals("executive")){
             nuEmp = new Executive();
         }else if(type.equals("hourly")){
@@ -66,20 +75,31 @@ public class Company {
         for(Employee e : employees){
             if(e.getName().equals(eName)){
                 removed = e;
-                employees.remove(e);
             }
         }
+        employees.remove(removed);
         String mess = (removed==null) ? "could not find employee or employee does not exist.":"successful";
         System.out.println(mess);
         return removed;
     }
     public void payEmployees(){
         for(Employee e : employees){
+
+            e.setHoursForWeek(Integer.parseInt(userInput("enter number of hours for employee "+e.getName()+":", ""), 10));
             System.out.println(e.getName() + " was payed: "+e.calcPay());
         }
     }
     public ArrayList<Employee> getEmployees(){
         return employees;
+    }
+    public void printEmployees(){
+        if(employees.isEmpty()){
+            System.out.println("no employees to list");
+        }else {
+            for (Employee e : employees) {
+                System.out.println(e.getClass().getSimpleName() + " " + e.getName());
+            }
+        }
     }
     public String userInput(String message, String type){
         Scanner scanner = new Scanner(System.in);
